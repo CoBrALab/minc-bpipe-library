b6601b424f387773d496c792ca61650d15bff8d9
Gabriel A. Devenyi <gdevenyi@gmail.com>
2015-06-17 13:58:10 -0400

    Add scinet wrapper

---
+++ bpipe-batch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#bpipe-batch is a program intended to wrap bpipe for use on the scinet gpc cluster
+#Invocation:
+#bpipe-batch.sh /path/to/my/pipeline <list of inputs> | qbatch 1 24:00:00
+#It should be invoked in the directory that will contain the output
+
+pipeline=$1
+shift
+args=( "$@" )
+
+echo ${args[@]} | parallel --no-notice --recend "" --delimiter ' ' -N8 "echo bpipe run -n 8 -m 14GB $1 {1} {2} {3} {4} {5} {6} {7} {8}" | awk NF
