a9ae767975f10cb080145e4e8a866eb00ec14535
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-08-17 21:57:24 -0400

    Update scinet QC script

--- generate-bpipe-QC.sh
+++ generate-bpipe-QC.sh
@@ -2,20 +2,22 @@
 file=$1
 tmpdir=$(mktemp -d)
 
+REGISTRATIONOUTLINE="/project/m/mchakrav/quarantine/resources/mni_icbm152_nlin_sym_09c_minc2/mni_icbm152_t1_tal_nlin_sym_09c_outline.mnc"
+
 create_verify_image $tmpdir/$(basename $file .mnc)_t.rgb \
 -width 1920 -autocols 6 -autocol_planes t \
 -row $file color:gray \
-volume_overlay:$(basename $file .mnc).beastmask.mnc
+volume_overlay:$(basename $file .mnc).beastmask.mnc volume_overlay:${REGISTRATIONOUTLINE}:1:blue
 
 create_verify_image $tmpdir/$(basename $file .mnc)_s.rgb \
 -width 1920 -autocols 6 -autocol_planes s \
 -row $file color:gray \
-volume_overlay:$(basename $file .mnc).beastmask.mnc
+volume_overlay:$(basename $file .mnc).beastmask.mnc volume_overlay:${REGISTRATIONOUTLINE}:1:blue
 
 create_verify_image $tmpdir/$(basename $file .mnc)_c.rgb \
 -width 1920 -autocols 6 -autocol_planes c \
 -row $file color:gray \
-volume_overlay:$(basename $file .mnc).beastmask.mnc
+volume_overlay:$(basename $file .mnc).beastmask.mnc volume_overlay:${REGISTRATIONOUTLINE}:1:blue
 
 convert -append $tmpdir/*.rgb $2
 
