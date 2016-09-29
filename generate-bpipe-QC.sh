1a7122ab9c90a0ae1bfc0cf0491ab65c99adf237
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-09-29 15:19:19 -0400

    Update script to allow running from anywhere

--- generate-bpipe-QC.sh
+++ generate-bpipe-QC.sh
@@ -7,17 +7,17 @@ REGISTRATIONOUTLINE="/project/m/mchakrav/quarantine/resources/mni_icbm152_nlin_s
 create_verify_image $tmpdir/$(basename $file .mnc)_t.rgb \
 -width 1920 -autocols 6 -autocol_planes t \
 -row $file color:gray \
-volume_overlay:$(basename $file .mnc).beastmask.mnc volume_overlay:${REGISTRATIONOUTLINE}:1:blue
+volume_overlay:$(dirname $file)/$(basename $file .mnc).beastmask.mnc volume_overlay:${REGISTRATIONOUTLINE}:1:blue
 
 create_verify_image $tmpdir/$(basename $file .mnc)_s.rgb \
 -width 1920 -autocols 6 -autocol_planes s \
 -row $file color:gray \
-volume_overlay:$(basename $file .mnc).beastmask.mnc volume_overlay:${REGISTRATIONOUTLINE}:1:blue
+volume_overlay:$(dirname $file)/$(basename $file .mnc).beastmask.mnc volume_overlay:${REGISTRATIONOUTLINE}:1:blue
 
 create_verify_image $tmpdir/$(basename $file .mnc)_c.rgb \
 -width 1920 -autocols 6 -autocol_planes c \
 -row $file color:gray \
-volume_overlay:$(basename $file .mnc).beastmask.mnc volume_overlay:${REGISTRATIONOUTLINE}:1:blue
+volume_overlay:$(dirname $file)/$(basename $file .mnc).beastmask.mnc volume_overlay:${REGISTRATIONOUTLINE}:1:blue
 
 convert -append $tmpdir/*.rgb $2
 
